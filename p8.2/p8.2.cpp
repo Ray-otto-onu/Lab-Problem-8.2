da36ebd010ff55b87ebe342e93b133bdfe7eb3c5
RAY OTTO <rjotto123@gmail.com>
2023-10-23 21:49:24 -0400

    Add project files.

---
+++ p8.2/p8.2.cpp
@@ -0,0 +1,28 @@
+#include<string>
+#include <iostream>
+
+using namespace std;
+
+
+int countWords(string str);
+
+int main(void) {
+	int length = 0;
+	string input;
+
+	getline(cin, input);
+	length = countWords(input);
+	cout << "Word count " << length << endl;
+}
+
+int countWords(string str) {
+	int length = 0;
+	int l = str.length();
+
+	for (int i = 0; i < l; i++) {
+		if (str[i] == ' ') {
+			length++;
+		}
+	}
+	return length+1;
+}
